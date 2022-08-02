0b7201c8ede8f1f794471beb7f4c92bbf0253574
Ozy <munkhtamir.g@outlook.com>
2022-08-02 16:34:38 +0800

    Initial

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const categoryRouter = require("./routes/categories");
+const PORT = process.env.PORT;
+const cors = require("cors");
+
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+
+app.use(express.json());
+app.use("/category", categoryRouter);
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.listen(PORT, () => {
+  console.log("Runnig -> " + PORT);
+});
